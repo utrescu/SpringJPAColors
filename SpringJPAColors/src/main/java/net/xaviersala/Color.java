056e79664291b5a129175a0749900d09300d7518
Xavier Sala <utrescu@gmail.com>
2015-04-17 16:20:59 +0200

    Primer commit

---
+++ SpringJPAColors/src/main/java/net/xaviersala/Color.java
@@ -0,0 +1,96 @@
+package net.xaviersala;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Color {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", updatable = false, nullable = false)
+  private Long id;
+  
+  protected String catala;
+  protected String castella;
+  protected String frances;
+  protected String angles;
+  
+  public Color() {
+    
+  }
+  
+  public Color(String string, String string2, String string3, String string4) {
+    catala = string;
+    castella = string2;
+    frances = string3;
+    angles = string4;
+  }
+  
+  public Long getId()
+  {
+     return this.id;
+  }
+
+  public void setId(final Long id)
+  {
+     this.id = id;
+  } 
+  
+  /**
+   * @return the catala
+   */
+  public String getCatala() {
+    return catala;
+  }
+  /**
+   * @param catala the catala to set
+   */
+  public void setCatala(String catala) {
+    this.catala = catala;
+  }
+  /**
+   * @return the castella
+   */
+  public String getCastella() {
+    return castella;
+  }
+  /**
+   * @param castella the castella to set
+   */
+  public void setCastella(String castella) {
+    this.castella = castella;
+  }
+  /**
+   * @return the frances
+   */
+  public String getFrances() {
+    return frances;
+  }
+  /**
+   * @param frances the frances to set
+   */
+  public void setFrances(String frances) {
+    this.frances = frances;
+  }
+  /**
+   * @return the angles
+   */
+  public String getAngles() {
+    return angles;
+  }
+  /**
+   * @param angles the angles to set
+   */
+  public void setAngles(String angles) {
+    this.angles = angles;
+  }
+  
+  public String toString() {
+    return catala + " " + castella + " " + frances + " " + angles;
+  }
+  
+}
---
+++ SpringJPAColors/src/test/java/net/xaviersala/SpringJpaColorsApplicationTests.java
@@ -0,0 +1,16 @@
+package net.xaviersala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringJpaColorsApplication.class)
+public class SpringJpaColorsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
